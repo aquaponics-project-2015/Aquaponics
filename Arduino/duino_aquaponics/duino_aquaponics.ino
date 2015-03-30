549ac8211b7b707db60ef310d41b0c3457aa8fa7
irpl <philo3194@gmail.com>
2015-03-30 00:40:18 -0500

    merged python scripts, pH code added, modularized

--- Arduino/duino_aquaponics/duino_aquaponics.ino
+++ Arduino/duino_aquaponics/duino_aquaponics.ino
@@ -2,38 +2,20 @@
 #include <DallasTemperature.h>
 #include <NewPing.h>
 
-#define TRIGGER_PIN_1  	12  // Arduino pin tied to trigger pin on the ultrasonic sensor.
-#define TRIGGER_PIN_2  	13  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define TRIGGER_PIN_1  	  12  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define TRIGGER_PIN_2  	  13  // Arduino pin tied to trigger pin on the ultrasonic sensor.
 
-#define ECHO_PIN_1     	10  // Arduino pin tied to echo pin on the ultrasonic sensor.
-#define ECHO_PIN_2     	11  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define ECHO_PIN_1     	  10  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define ECHO_PIN_2     	  11  // Arduino pin tied to echo pin on the ultrasonic sensor.
 
-#define MAX_DISTANCE 	200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+#define MAX_DISTANCE 	  200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 
-#define SENSOR_PIN	2
+#define SENSOR_PIN	  2
 
-/*
-https://www.pjrc.com/teensy/td_libs_OneWire.html
-http://www.hobbytronics.co.uk/ds18b20-arduino
-https://code.google.com/p/arduino-new-ping/
-*/
-
-OneWire oneWire(SENSOR_PIN);
-
-DallasTemperature sensor(&oneWire);
-#include <OneWire.h>
-#include <DallasTemperature.h>
-#include <NewPing.h>
-
-#define TRIGGER_PIN_1  	12  // Arduino pin tied to trigger pin on the ultrasonic sensor.
-#define TRIGGER_PIN_2  	13  // Arduino pin tied to trigger pin on the ultrasonic sensor.
-
-#define ECHO_PIN_1     	10  // Arduino pin tied to echo pin on the ultrasonic sensor.
-#define ECHO_PIN_2     	11  // Arduino pin tied to echo pin on the ultrasonic sensor.
-
-#define MAX_DISTANCE 	200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
-
-#define SENSOR_PIN	2
+#define SensorPin         A0            //pH meter Analog output to Arduino Analog Input 0
+#define Offset            0.00            //deviation compensate
+#define samplingInterval  20
+#define ArrayLenth        40    //times of collection
 
 /*
 https://www.pjrc.com/teensy/td_libs_OneWire.html
@@ -42,11 +24,11 @@ https://code.google.com/p/arduino-new-ping/
 */
 
 OneWire oneWire(SENSOR_PIN);
-
 DallasTemperature sensor(&oneWire);
-
 NewPing sonar1(TRIGGER_PIN_1, ECHO_PIN_1, MAX_DISTANCE);
 NewPing sonar2(TRIGGER_PIN_2, ECHO_PIN_2, MAX_DISTANCE);
+int pHArray[ArrayLenth];   //Store the average value of the sensor feedback
+int pHArrayIndex=0;
 
 void setup()
 {
@@ -56,86 +38,117 @@ void setup()
 
 void loop()
 {
-	unsigned int uS_1 = sonar1.ping();
-	unsigned int uS_2 = sonar2.ping();
-	float left_water_level = uS_1 / US_ROUNDTRIP_CM;
-	float right_water_level = uS_2 / US_ROUNDTRIP_CM;
-	float avgWaterLevel = (left_water_level + right_water_level) / 2;
+  float avgWaterlevel = getWaterLevel();
+  float phVal = getpH();
+  float avgTemp = getTemp();
+  
+  Serial.print(avgWaterlevel);
+  Serial.print(",");
+  Serial.print(phVal);
+  Serial.print(",");
+  Serial.print(avgTemp);
+  Serial.print(",");
+  if(isPump())
+    Serial.print("On");  
+  else
+    Serial.print("Off");  
 
-	float currentTemp1, currentTemp2;
-	sensor.requestTemperatures();
-	currentTemp1 = sensor.getTempCByIndex(0);
-	currentTemp2 = sensor.getTempCByIndex(1);
-
-	float avgTemp = (currentTemp1 + currentTemp1) / 2;
-
-	if(abs(left_water_level - avgWaterLevel) > 10 
-		|| abs(right_water_level - avgWaterLevel) > 10)
-	{
-		//smn wrong
-	}
-
-	if(abs(currentTemp1 - avgTemp) > 3 
-		|| abs(currentTemp2 - avgTemp) > 3)
-	{
-		//smn wrong
-	}
-
-	//test code, change print to write
-	Serial.print("current average temperature: \t");
-	Serial.print(avgTemp);
-	Serial.println("degC");
-
-	Serial.print("average water level: \t\t");
-	Serial.print(avgWaterLevel);
-	Serial.println("cm\n");
-
-	delay(1000);
-}
-NewPing sonar1(TRIGGER_PIN_1, ECHO_PIN_1, MAX_DISTANCE);
-NewPing sonar2(TRIGGER_PIN_2, ECHO_PIN_2, MAX_DISTANCE);
-
-void void setup()
-{
-	sensor.begin();	
-	Serial.begin(9600);
+  delay(1000);
 }
 
-void loop()
+float getWaterLevel()
 {
-	unsigned int uS_1 = sonar1.ping();
-	unsigned int uS_2 = sonar2.ping();
-	float left_water_level = uS_1 / US_ROUNDTRIP_CM;
-	float right_water_level = uS_2 / US_ROUNDTRIP_CM;
-	float avgWaterLevel = (left_water_level + right_water_level) / 2;
+  unsigned int uS_1 = sonar1.ping();
+  unsigned int uS_2 = sonar2.ping();
+  float left_water_level = uS_1 / US_ROUNDTRIP_CM;
+  float right_water_level = uS_2 / US_ROUNDTRIP_CM;
+  float avgWaterLevel = (left_water_level + right_water_level) / 2;
+  
+  if(abs(left_water_level - avgWaterLevel) > 10
+    || abs(right_water_level - avgWaterLevel) > 10)
+  {
+    //smn wrong
+  }
+  return avgWaterLevel;
+}
 
-	float currentTemp1, currentTemp2;
-	tempSensor.requestTemperature();
-	currentTemp1 = sensor.getTempCBYIndex(0);
-	currentTemp2 = sensor.getTempCBYIndex(1);
+float getTemp()
+{
+  float currentTemp1, currentTemp2;
+  sensor.requestTemperatures();
+  currentTemp1 = sensor.getTempCByIndex(0);
+  currentTemp2 = sensor.getTempCByIndex(1);
 
-	float avgTemp = (currentTemp1 + currentTemp1) / 2;
+  float avgTemp = (currentTemp1 + currentTemp1) / 2;
+  
+  if(abs(currentTemp1 - avgTemp) > 3 
+    || abs(currentTemp2 - avgTemp) > 3)
+  {
+    //smn wrong
+  }
+  return avgTemp;
+}
 
-	if(abs(left_water_level - avgWaterLevel) > 10 
-		|| abs(right_water_level - avgWaterLevel) > 10)
-	{
-		//smn wrong
-	}
-
-	if(abs(currentTemp1 - avgTemp) > 3 
-		|| abs(currentTemp2 - avgTemp) > 3)
-	{
-		//smn wrong
-	}
-
-	//test code, change print to write
-	Serial.print("current average temperature: \t")
-	Serial.print(avgTemp);
-	Serial.println("degC\n");
-
-	Serial.print("average water level: \t");
-	Serial.print(avgWaterLevel);
-	Serial.println("cm\n");
-
-	delay(1000);
+float getpH()
+{
+  static unsigned long samplingTime = millis();
+  static unsigned long printTime = millis();
+  static float pHValue,voltage;
+  if(millis()-samplingTime > samplingInterval)
+  {
+      pHArray[pHArrayIndex++]=analogRead(SensorPin);
+      if(pHArrayIndex==ArrayLenth)pHArrayIndex=0;
+      voltage = avergearray(pHArray, ArrayLenth)*5.0/1024;
+      pHValue = 3.5*voltage+Offset;
+      samplingTime=millis();
+  }
+  //if(millis() - printTime > printInterval)   //Every 800 milliseconds, print a numerical, convert the state of the LED indicator
+  //{
+	//Serial.print("Voltage:");
+       // Serial.print(voltage,2);
+        //Serial.print("    pH value: ");
+	//Serial.println(pHValue);
+  return pHValue;
+        //digitalWrite(LED,digitalRead(LED)^1);
+        //printTime=millis();
+  //}
+}
+double avergearray(int* arr, int number){
+  int i;
+  int max,min;
+  double avg;
+  long amount=0;
+  if(number<=0){
+    Serial.println("Error number for the array to avraging!/n");
+    return 0;
+  }
+  if(number<5){   //less than 5, calculated directly statistics
+    for(i=0;i<number;i++){
+      amount+=arr[i];
+    }
+    avg = amount/number;
+    return avg;
+  }else{
+    if(arr[0]<arr[1]){
+      min = arr[0];max=arr[1];
+    }
+    else{
+      min=arr[1];max=arr[0];
+    }
+    for(i=2;i<number;i++){
+      if(arr[i]<min){
+        amount+=min;        //arr<min
+        min=arr[i];
+      }else {
+        if(arr[i]>max){
+          amount+=max;    //arr>max
+          max=arr[i];
+        }else{
+          amount+=arr[i]; //min<=arr<=max
+        }
+      }//if
+    }//for
+    avg = (double)amount/(number-2);
+  }//if
+  return avg;
 }
--- Python/pushData.py
+++ Python/pushData.py
@@ -1,13 +1,15 @@
 import serial
 import sys
+import requests
+import datetime
 
-ser = serial.Serial('COM4', 9600, timeout = 0)
+baseURL = "ServerIP"
+ser = serial.Serial('COM#', 9600, timeout = 0)
 
 def main():
     while True:
         while(ser.inWaiting() == 0):
             pass
-        
         val = getValue()
         print val
 
@@ -22,4 +24,17 @@ def getValue():
             val+=(byte)
     return val
 
+def postEnvToServer(temp ,ph ,waterlevel):
+	endpointURL = baseURL+#The api endpoint
+	date=datetime.datetime.now()
+	payload = {"waterLevel":waterlevel,"ph":ph,"temperature":temp,"datetime":date}
+	r = requests.post(endpointURL, data = payload)
+	print r.status_code
+
+def postPumpToServer(status):
+	endpointURL = baseURL+#The api endpoint
+	date=datetime.datetime.now()
+	payload = {"status":status, "datetime":date}
+	r = requests.post(endpointURL, data = payload)
+	print r.status_code
 main()
